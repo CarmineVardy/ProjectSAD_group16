f215c31e165a288f6c16f8f405b2acffa44ffc6e
CarmineVardy <150947253+CarmineVardy@users.noreply.github.com>
2025-05-15 13:30:00 +0200

    US01.3 end US02.1

--- GeoShapes/src/main/java/com/example/geoshapes/controller/MainController.java
+++ GeoShapes/src/main/java/com/example/geoshapes/controller/MainController.java
@@ -1,14 +1,12 @@
 package com.example.geoshapes.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ToggleGroup tools;
+
+
 }
